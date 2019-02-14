7efe03e39f8495b2d614bbc18a357eef8cfb9e25
otsuki <takashi_otsuki@outlook.com>
2019-02-14 14:25:27 +0900

    Add constructor Estimate(Content).

--- src/org/aiwolf/sample/player/Estimate.java
+++ src/org/aiwolf/sample/player/Estimate.java
@@ -13,6 +13,8 @@ import org.aiwolf.client.lib.AndContentBuilder;
 import org.aiwolf.client.lib.BecauseContentBuilder;
 import org.aiwolf.client.lib.Content;
 import org.aiwolf.client.lib.EstimateContentBuilder;
+import org.aiwolf.client.lib.Operator;
+import org.aiwolf.client.lib.Topic;
 import org.aiwolf.client.lib.XorContentBuilder;
 import org.aiwolf.common.data.Agent;
 import org.aiwolf.common.data.Role;
@@ -28,15 +30,39 @@ class Estimate {
 	private List<Role> roles = new ArrayList<>();
 	private List<Content> reasons = new ArrayList<>();
 
-	Estimate(Agent estimater, Agent estimated, Role role) {
+	private void setEstimater(Agent estimater) {
 		this.estimater = estimater;
+	}
+
+	private void setEstimated(Agent estimated) {
 		this.estimated = Agent.UNSPEC == estimated ? Agent.ANY : estimated;
+	}
+
+	Estimate(Agent estimater, Agent estimated, Role role) {
+		setEstimater(estimater);
+		setEstimated(estimated);
 		roles.add(role);
 	}
 
 	Estimate(Agent estimater, Agent estimated, Role role, Content reason) {
 		this(estimater, estimated, role);
 		addReason(reason);
+	}
+
+	Estimate(Content content) {
+		if (content.getTopic() == Topic.ESTIMATE) {
+			setEstimater(content.getSubject());
+			setEstimated(content.getTarget());
+			addRole(content.getRole());
+		} else if (content.getOperator() == Operator.BECAUSE && content.getContentList().get(1).getTopic() == Topic.ESTIMATE) {
+			Content estimate = content.getContentList().get(1);
+			Content reason = content.getContentList().get(0);
+			setEstimater(estimate.getSubject());
+			setEstimated(estimate.getTarget());
+			addRole(estimate.getRole());
+			addReason(reason);
+		}
+
 	}
 
 	void addRole(Role role) {
